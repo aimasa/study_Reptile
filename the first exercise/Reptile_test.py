4ddf8a7b8236fa975ec8cda81518433cedf48943
zhy <794182811@qq.com>
2019-05-10 15:35:10 +0800

    install了selenium，调用浏览器搜索百度网站

---
+++ the first exercise/Reptile_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3 
+# -*- coding: utf-8 -*- 
+# @Time : 2019/5/8 9:17 
+# @Author : luliangwei 
+# @Site :  
+# @File : Reptile_test.py 
+# @Software: PyCharm
+
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get('https://www.baidu.com')
